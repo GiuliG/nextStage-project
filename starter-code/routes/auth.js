c2bfa7a241d91519b97fa6c8ced8fc3378d4ae54
MarloMorgan <granatogi@gmail.com>
2018-11-24 11:05:59 +0100

    login works

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 // Get signup page for attendees
 router.get('/signup', (req, res, next) => {
@@ -10,17 +12,31 @@ router.get('/signup', (req, res, next) => {
 
 router.post('/signup', (req, res, next) => {
   const { email, password } = req.body;
+
   const user = {
     email,
     password,
     role: 'Attendee'
   };
-  User.create(user)
+  User.findOne({ email })
     .then((user) => {
-      console.log('created user');
-      res.redirect('/');
-    }
-    )
+      if (user) {
+      // req.flash('Error', 'User already taken');
+        return res.redirect('/auth/signup');
+      }
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      User.create({
+        email,
+        password: hashedPassword,
+        role: 'Attendee'
+      })
+        .then((newUser) => {
+          // req.session.currentUser = newUser;
+          res.redirect('/');
+        })
+        .catch(next);
+    })
     .catch(next);
 });
 
@@ -74,6 +90,31 @@ router.post('/signup-perform', (req, res, next) => {
       res.redirect('/');
     }
     )
+    .catch(next);
+});
+
+// login form
+router.get('/login', (req, res, next) => {
+  res.render('auth/login');
+});
+
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        // req.flash('Error', 'User name or password incorrect');
+        console.log('here');
+        return res.redirect('/auth/login');
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        // req.session.currentUser = user;
+        res.redirect('/');
+      } else {
+        console.log('there');
+        res.redirect('/auth/login');
+      }
+    })
     .catch(next);
 });
 
