0e1ff7979338556fa522fb615772d66e252d12ff
MarloMorgan <granatogi@gmail.com>
2018-11-28 15:32:48 +0100

    remaining tickets work

--- routes/events.js
+++ routes/events.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const Event = require('../models/events');
 const Request = require('../models/request');
+const authMiddleware = require('../middleware/authmiddleware');
 
 router.post('/:requestId', (req, res, next) => {
   const requestId = req.params.requestId;
@@ -21,6 +22,29 @@ router.post('/:requestId', (req, res, next) => {
           res.redirect('/lists/my-requests');
         })
         .catch(next);
+    })
+    .catch(next);
+});
+
+router.post('/:eventId/attend', authMiddleware.requireUser, (req, res, next) => {
+  const eventId = req.params.eventId;
+  const userId = req.session.currentUser._id;
+  Event.findByIdAndUpdate(eventId, { $push: { attendees: userId } }, { new: true })
+    .then((result) => {
+      console.log(result);
+      return res.redirect('/#events');
+    })
+    .catch(next);
+});
+
+// GET list of events by user
+router.get('/attendee-profile', (req, res, next) => {
+  const { _id } = req.session.currentUser;
+  Event.find({ attendee: _id })
+    .populate('hostId').populate('artistId')
+    .then((result) => {
+      console.log(result);
+      res.render('/attendee-profile', { events: result });
     })
     .catch(next);
 });
