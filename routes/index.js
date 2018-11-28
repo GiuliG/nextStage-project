b6e454699cf4447098c5e2c6fe5ad41219f79353
MarloMorgan <granatogi@gmail.com>
2018-11-28 17:00:12 +0100

    date added to the event

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,14 @@ const Event = require('../models/events');
 router.get('/', (req, res, next) => {
   // Get info from all events
   Event.find()
-    .populate('hostId').populate('artistId')
+    .populate('hostId')
+    .populate('artistId')
     .then((results) => {
       console.log(results);
       for (let i = 0; i < results.length; i++) {
         for (let j = 0; j < results[i].attendees.length; j++) {
           if (req.session.currentUser && results[i].attendees[j].equals(req.session.currentUser._id)) {
             results[i].isAttending = true;
-          } else {
-            results[i].isAttending = false;
           }
           results[i].avalible = results[i].hostId.host.roomCapacity - results[i].attendees.length;
         }
