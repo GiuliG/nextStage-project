07654d4d7dca81205442b10fc3db0591771f071e
MarloMorgan <granatogi@gmail.com>
2018-11-30 15:22:22 +0100

    fixed tickets again

--- routes/index.js
+++ routes/index.js
@@ -13,19 +13,18 @@ router.get('/', (req, res, next) => {
     .then((results) => {
       for (let i = 0; i < results.length; i++) {
         console.log(results[i].attendees);
-        if (!results[i].attendees.length === 0) {
+        if (results[i].attendees.length > 0) {
           for (let j = 0; j < results[i].attendees.length; j++) {
             if (req.session.currentUser && results[i].attendees[j].equals(req.session.currentUser._id)) {
               results[i].isAttending = true;
             }
             results[i].available = results[i].hostId.host.roomCapacity - results[i].attendees.length;
-            console.log(results[i].available);
+            console.log(results[i].isAttending);
           }
         } else {
           results[i].available = results[i].hostId.host.roomCapacity;
         }
       }
-
       res.render('index', { events: results });
     })
     .catch(next);
